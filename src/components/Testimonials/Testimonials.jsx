f4a1bbc17eb83649d56ea440a1541499d507ff6d
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2020-12-29 00:36:41 -0500

    Testimonials Extracted

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,4 +1,5 @@
 import Faker from 'faker'
+import TestimonialsList from './Testimonals.json'
 
 export default function Testimonials() {
     return (
@@ -10,39 +11,7 @@ export default function Testimonials() {
                         <p className='text-lg'>Don't listen to us — listen to our clients.</p>
                     </div>
                     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
-                        {[
-                            {
-                                date: '6 months ago',
-                                message:
-                                    'Been playing Apex Legends for weeks with LastCheats mods... almost forgot I had them!',
-                                name: Faker.name.findName()
-                            },
-                            {
-                                date: '3 months ago',
-                                message: 'Fugiat adipisicing dolore consectetur proident laboris.',
-                                name: Faker.name.findName()
-                            },
-                            {
-                                date: '2 hours ago',
-                                message: 'Duis nostrud magna consequat fugiat tempor aliquip.',
-                                name: Faker.name.findName()
-                            },
-                            {
-                                date: '1 month ago',
-                                message: 'Ullamco fugiat amet tempor qui quis.',
-                                name: Faker.name.findName()
-                            },
-                            {
-                                date: '6 hours ago',
-                                message: 'Sint enim dolore officia nulla nulla sunt nisi incididunt eiusmod nulla.',
-                                name: Faker.name.findName()
-                            },
-                            {
-                                date: '1 hour ago',
-                                message: 'Ad culpa est nostrud veniam ad.',
-                                name: Faker.name.findName()
-                            }
-                        ].map((testimonial, index) => (
+                        {TestimonialsList.map((testimonial, index) => (
                             <div
                                 className='w-full p-6 bg-yellow-400 text-yellow-800 shadow-xl rounded-lg flex flex-col'
                                 key=''>
