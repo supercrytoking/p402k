b5079cb5983ce234da81cba7737223b7b33e4987
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2020-12-29 00:44:21 -0500

    Added Meta

---
+++ src/components/Meta.jsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+const Meta = () => (
+    <Head>
+        <title>LastCheat &bull; Top-quality Game Hacks</title>
+        <meta name='description' content='LastCheat —  Top-quality Game Hacks' />
+        <meta property='og:type' content='website' />
+        <meta name='og:title' property='og:title' content='LastCheat' />
+        <meta name='og:description' property='og:description' content='LastCheat —  Top-quality Game Hacks' />
+        <meta property='og:site_name' content='LastCheat' />
+        <meta property='og:url' content='https://lastcheat.com' />
+        <meta name='twitter:card' content='summary' />
+        <meta name='twitter:title' content='LastCheat' />
+        <meta name='twitter:description' content='LastCheat —  Top-quality Game Hacks' />
+        <meta name='twitter:site' content='https://lastcheat.com' />
+        <meta property='og:image' content='/img/apex.png' />
+        <meta name='twitter:image' content='https://p402k.vercel.app/img/apex.png' />
+    </Head>
+)
+
+export default Meta
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
+import Meta from '../components/Meta'
 import Nav from '../components/Nav'
 import '../global.scss'
 
 export default function App({ Component, pageProps }) {
     return (
         <>
+            <Meta />
             <Nav />
             <Component {...pageProps} />
         </>
